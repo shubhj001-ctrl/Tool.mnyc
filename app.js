877e3ac1c73786531f7efb1845fa51a9845b3f02
shubhj001-ctrl <shubhamjaggi44@gmail.com>
2026-01-22 01:39:52 +0530

    fixes minor logic issues 2.1

--- app.js
+++ app.js
@@ -2148,7 +2148,7 @@ function populateAgentDropdown() {
   
   allUsers.forEach((user) => {
     const option = document.createElement('option');
-    option.value = user.id;
+    option.value = user.odoo_id;
     option.textContent = user.name;
     select.appendChild(option);
   });
